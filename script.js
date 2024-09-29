c78413648e51d13a31c161df98dc9d18b44f959a
vaibha987 <v2goyal95@gmail.com>
2024-09-29 18:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const text=document.getElementById("text")
+const delay=document.getElementById("delay")
+const button=document.getElementById("btn")
+const output = document.getElementById("output")
+button.addEventListener('click',async ()=>{
+	const promise = new Promise((resolve, reject) => {
+	setTimeout(()=>{
+	resolve(text.value)	
+	},delay.value)
+})
+output.innerHTML = await promise;
+})
